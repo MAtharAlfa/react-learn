21ddfbb1ab58197fc2a7666adc3b897af1b8af7e
MAtharAlfa <alfaathar1311@gmail.com>
2025-11-01 15:23:57 +0700

    conditonal dynamic rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
+  let items = ["New York", "San Francisco", "London", "Tokyo", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List Group</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
